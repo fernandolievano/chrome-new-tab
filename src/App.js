c2cb3c289fc85141f5be9dfa677ceb85670a0ffc
fernandolievano <fernandolievan@gmail.com>
2024-02-12 21:49:55 -0300

    feat: styling .main container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import './App.css'
+
 function App() {
   return (
     <div className="main">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, libero sunt eligendi voluptatibus nam quis quidem rerum eaque ad! Exercitationem corporis at laborum nostrum delectus voluptate quod. Molestias, facilis fuga.
+      New Tab - Chrome Extension
     </div>
   );
 }
