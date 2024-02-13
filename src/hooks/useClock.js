2447271a2ba13d3ea9e7c2b3d09537c213592d9b
fernandolievano <fernandolievan@gmail.com>
2024-02-13 20:45:42 -0300

    feature: show current time

---
+++ src/hooks/useClock.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+
+const useClock = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  let interval;
+
+  const tick = () => {
+    setCurrentTime(new Date());
+  };
+
+  useEffect(() => {
+    interval = setInterval(() => {
+      tick();
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
+  const formatMinutes = (val) => {
+    if (val < 10) {
+      val = "0" + val;
+    }
+    return val;
+  };
+
+  const prettifiedTime = () => {
+    return `${currentTime.getHours()} : ${formatMinutes(currentTime.getMinutes())}`
+  }
+
+  return {
+    prettifiedTime
+  };
+};
+
+export default useClock;
