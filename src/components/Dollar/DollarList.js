9f231bbc47afa27fd400aa5051880cab38f0a782
fernandolievano <fernandolievan@gmail.com>
2024-04-18 22:10:52 -0300

    feat: loadin/error messages

--- src/components/Dollar/DollarList.js
+++ src/components/Dollar/DollarList.js
@@ -3,39 +3,55 @@ import useDollarPrice from 'hooks/useDollarPrice';
 import './dollar.css';
 
 const DollarList = () => {
-  const { dollarPrices } = useDollarPrice();
-  return (
-    <div className="overflow-hidden flex py-1 px-0 relative gap-4 select-none w-full">
-      <div className="marquee flex flex-shrink-0 justify-around gap-[15px]">
-        {
-          dollarPrices.map(price => {
-            return (
-              <div className="py-1" key={price.casa}>
-                <div className={(price.casa === 'oficial' ? 'text-[#0F9D58]' : 'text-gray-700')}>
-                  <span>{price.nombre}:</span>{' '}<strong>${price.venta}</strong>
-                </div>
-              </div>
-            );
-          })
-        }
-
+  const { dollarPrices, dollarErrors, isLoading } = useDollarPrice();
+  if (isLoading) {
+    return (
+      <div className="flex py-1 px-0 w-full text-gray-600">
+        Cargando...
       </div>
-      <div className="marquee flex flex-shrink-0 justify-around gap-[15px]" aria-hidden="true">
-        {
-          dollarPrices.map(price => {
-            return (
-              <div className="py-1" key={price.casa}>
-                <div className={(price.casa === 'oficial' ? 'text-[#0F9D58]' : 'text-gray-700')}>
-                  <span>{price.nombre}:</span>{' '}<strong>${price.venta}</strong>
-                </div>
-              </div>
-            );
-          })
-        }
+    )
+  } else {
+    if (dollarErrors) {
+      return (
+        <div className="flex py-1 px-0 w-full text-red-500">
+          No se pudieron cargar los datos
+        </div>
+      )
+    } else {
+      return (
+        <div className="overflow-hidden flex py-1 px-0 relative gap-4 select-none w-full">
+          <div className="marquee flex flex-shrink-0 justify-around gap-[15px]">
+            {
+              dollarPrices.map(price => {
+                return (
+                  <div className="py-1" key={price.casa}>
+                    <div className={(price.casa === 'oficial' ? 'text-[#0F9D58]' : 'text-gray-700')}>
+                      <span>{price.nombre}:</span>{' '}<strong>${price.venta}</strong>
+                    </div>
+                  </div>
+                );
+              })
+            }
 
-      </div>
-    </div>
-  );
+          </div>
+          <div className="marquee flex flex-shrink-0 justify-around gap-[15px]" aria-hidden="true">
+            {
+              dollarPrices.map(price => {
+                return (
+                  <div className="py-1" key={price.casa}>
+                    <div className={(price.casa === 'oficial' ? 'text-[#0F9D58]' : 'text-gray-700')}>
+                      <span>{price.nombre}:</span>{' '}<strong>${price.venta}</strong>
+                    </div>
+                  </div>
+                );
+              })
+            }
+
+          </div>
+        </div>
+      );
+    }
+  }
 };
 
 export default DollarList;
--- src/hooks/useDollarPrice.js
+++ src/hooks/useDollarPrice.js
@@ -3,22 +3,28 @@ import { useEffect, useState } from 'react';
 const useDollarPrice = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [dollarPrices, setDollarPrices] = useState([]);
+  const [dollarErrors, setDollarErrors] = useState(null);
 
   useEffect(() => {
     setIsLoading(true);
 
-    fetch("https://dolarapi.com/v1/dolares")
+    try {
+      fetch("https://dolarapi.com/v1/dolares")
       .then(response => response.json())
       .then(data => {
         console.log(data);
         setDollarPrices(data);
         setIsLoading(false);
       });
+    } catch (error) {
+      setDollarErrors(error)
+    }
   }, []);
 
   return {
     isLoading,
-    dollarPrices
+    dollarPrices,
+    dollarErrors
   };
 };
 
