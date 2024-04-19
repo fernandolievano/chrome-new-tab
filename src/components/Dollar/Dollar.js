d79591103ac1da1195b1c6b12b2958a88504740b
fernandolievano <fernandolievan@gmail.com>
2024-04-18 22:54:11 -0300

    css tweaks

--- src/components/Dollar/Dollar.js
+++ src/components/Dollar/Dollar.js
@@ -4,7 +4,7 @@ import DollarList from './DollarList';
 const Dollar = () => {
   return (
     <div id="dollar" className="bg-white fixed bottom-0 left-0 w-full  flex flex-shrink-0 items-stretch justify-start">
-      <h1 className="font-bold uppercase bg-[#0F9D58] text-green-300 py-1 px-3 flex items-center">Dólar</h1>
+      <h1 className="font-bold uppercase bg-[#0F9D58] text-green-200 py-1 px-3 flex items-center">Dólar</h1>
       <DollarList />
     </div>
   );
--- src/components/Wallpaper/Wallpaper.js
+++ src/components/Wallpaper/Wallpaper.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import useRandomWallpaper from 'hooks/useRandomWallpaper';
+import './wallpaper.css';
 
 const Wallpaper = () => {
   const { wallpaper } = useRandomWallpaper();
 
 
   return (
-    <div className={`text-white absolute top-3 left-3 text-xs ${wallpaper.copyright ? 'visible' : 'hidden'}`}>
+    <div className={`text-white wallpaper-att absolute top-3 left-3 text-xs ${wallpaper.copyright ? 'visible' : 'hidden'}`}>
       <a className='flex gap-2 items-center' href={wallpaper.copyright_link} target="_blank" rel="noopener noreferrer">
         <img className='w-4' src="/imageicon.png" alt="image icon" title="Icon by Fathema Khanom" />
         <strong>{wallpaper.copyright}</strong>
