f44270b6156f63802f4cded6b57de4c00fc0be48
fernandolievano <fernandolievan@gmail.com>
2025-05-31 21:02:34 -0300

    feat: configure vitest with setup file and add jest-dom support

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+import '@testing-library/jest-dom';
+
+declare module 'vitest' {
+	interface Assertion {
+		toBeInTheDocument(): void;
+	}
+}
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
+	plugins: [react()],
 	test: {
 		globals: true,
 		environment: 'jsdom',
-		setupFiles: './setupTests.js'
+		setupFiles: './vitest.setup.ts',
+		exclude: [...configDefaults.exclude, 'e2e']
 	}
 });
